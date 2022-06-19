891c76d48ff70de4a4cefd5d790c1a48e31ce57b
Naing Ba <tranksntb@gmail.com>
2022-06-19 12:48:00 +1000

    refactor

--- obsolete_codes.py
+++ obsolete_codes.py
@@ -21,3 +21,38 @@
 
 
 ####################################################################################################################
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+import time
+import csv
+
+options = Options()
+options.add_argument('--headless') # headless browser so GUI is not shown
+options.add_argument('--incognito')
+driver = webdriver.Chrome(options=options)
+# driver = webdriver.Chrome()
+
+
+url = 'https://www.neds.com.au/greyhound-racing/international'
+driver.get(url)
+time.sleep(2)
+try:
+    
+    elem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "page-content"))
+    )
+finally:
+    data = elem.text
+
+print(data)
+driver.close()
+
+
+# timestr = time.strftime("%Y%m%d")
+# print(timestr)
+
