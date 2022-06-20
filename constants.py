111b4c321f1fd2005cf4006d94d468a836f60d31
Naing Ba <tranksntb@gmail.com>
2022-06-20 23:25:06 +1000

    add more features
    can now place bet only by giving liability_amount in myBot_manual_export.py. Still need to manually time.

---
+++ constants.py
@@ -0,0 +1,8 @@
+## Constants and login details
+
+USERNAME = "mightytranks@gmail.com"
+PASSWORD = "OsGiwuevzRf4925Lfty!"
+API_KEY_DEMO = "X9EGwt5zbpg8o5xk"
+API_KEY_LIVE = "XzTt9hf8DAsmOuUa"
+
+GH_RACING_ID = 4339
