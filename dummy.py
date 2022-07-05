21555386b2b06db00778cab7a7e0517262fed465
Naing Ba <tranksntb@gmail.com>
2022-07-06 00:39:52 +1000

    some major fixes
    fav_price fix, etc.

---
+++ dummy.py
@@ -0,0 +1,5 @@
+import time
+
+for i in range(100):
+    print("hello", i)
+    time.sleep(1)
--- utils.py
+++ utils.py
@@ -12,6 +12,10 @@ import datetime
 import pytz
 import csv
 
+#########################################################
+## External functions
+#########################################################
+
 ###
 # function from betfair tutorial
 ###
@@ -63,7 +67,85 @@ def process_runner_books(runner_books):
         'Adjustment Factor': adjustment_factors
     })
     return df
+
+###
+# Returns the exacta from NEDS
+###
+def choose_lay_option_neds(venueName):
+    odds_arr = get_odds(venueName) # try getting the odds
+    if(len(odds_arr) == 0): # try a second time if result is empty
+        odds_arr = get_odds(venueName)
+    print(odds_arr)
+
+    ## Check if there are 6 runners
+    if len(odds_arr) != 6:
+        print('Length of odds_arr = ' + str(len(odds_arr)))
+        return -2
+
+    ## Sort out the odds_arr
+    sorted_odds = sorted(odds_arr,key=float)
+    print(sorted_odds)
+    pos1 = odds_arr.index(sorted_odds[0]) 
+    pos2 = odds_arr.index(sorted_odds[1]) 
+    pos3 = odds_arr.index(sorted_odds[2]) 
+    print("Forecast = " + str(pos1+1) + "-" + str(pos2+1))
+
+    ## TODO: check only if the 2 lowest odds are similar
+    if pos1 == pos2 or pos2 == pos3:
+        print('Two of the 3 smallest odds are the same : pos1 = ' + str(pos1) + ',pos2 = ' + str(pos2)+ ',pos3 = ' + str(pos3))
+        return -1
+
+    ## Choose the index of the lay selection out of 30 options or less
+    coordinates = []
+
+    for x in range(6):
+        for y in range(6):
+            if(x != y):
+                coordinates.append((x, y))
+
+    # res = [x+1 for x,y in enumerate(coordinates) if (y[0] ==pos1 and y[1] == pos2) ]
+    res = [x for x,y in enumerate(coordinates) if (y[0] ==pos1 and y[1] == pos2) ]
+    print(res[0])
+    return res[0]
+
+###
+# Get details of the next game to bet on
+###
+def get_next_market(market_catalogues):
+    timeNow = (datetime.datetime.now(pytz.timezone("Europe/London"))-datetime.timedelta(hours=1)) ## minus 1 hr due to daylight savings maybe?
+    print("Time Now: ")
+    print(timeNow.strftime("%Y-%m-%d %T"))
+    ## TODO Compare the list of markets - if timeNow is less than market start time (ie. market hasn't started yet), pick it. Otherwise, skip
+    time1 = timeNow.replace(tzinfo=pytz.UTC)
+
+    myRaceID = 0
+    myRaceVenue = ""
+    for marketObj in market_catalogues:
+        time2 = marketObj.market_start_time.replace(tzinfo=pytz.UTC)
+        if time1 < time2:
+            ## Found the race to bet
+            myRaceID = marketObj.market_id # store the market id
+            myRaceVenue = marketObj.event.venue.lower().replace(" ", "-") # store the venue
+            print("Found the market to lay: Name = " + myRaceVenue + " id = " + str(myRaceID))
+
+            break
+        
+    print("Market Start Time: " + str(time2))
     
+    ##TODO Sleep until x seconds before the start time
+    return time2, time2-time1, myRaceID, myRaceVenue
+
+###
+# Get the runner in box 1 
+# This is only used to verify the races from betfair and neds are identical
+###
+def get_runner1_name(market_catalogues):
+    return market_catalogues[0].runners[0].runner_name
+
+#########################################################
+## Internal functions
+#########################################################
+
 def get_neds_odds(venueName): 
     options = Options()
     options.add_argument('--headless') # headless browser so GUI is not shown
@@ -74,6 +156,7 @@ def get_neds_odds(venueName):
 
     # venueName = 'newcastle-bags'
     url = "https://www.neds.com.au/racing/" + venueName
+    print("Next Neds URL to try = ", url)
     driver.get(url)
     time.sleep(2)
     try:
@@ -90,14 +173,8 @@ def get_neds_odds(venueName):
     flag_part2 = False 
     data_arr_part1 = [] 
     data_arr_part2 = []
-    abdn_flag = False
     ## Check through the scraped text data
     for line in data.splitlines():
-        if(line == 'Abandoned'):
-            abdn_flag = True
-            print("abandon_flag = True")
-            break
-
         if not flag_part2:
             data_arr_part1.append(str(''.join(line)))
         if(line == 'RUNNERS' or flag_part2 == True):
@@ -108,8 +185,6 @@ def get_neds_odds(venueName):
     print(data_arr_part1)
     print("end of part1")
     print(data_arr_part2)
-
-    # assert(abdn_flag==True) # exit if the chosen race is abandoned
 
 
     ##############
@@ -166,96 +241,6 @@ def get_odds(venueName):
         time.sleep(3)
 
     return odds_arr
-
-def choose_lay_option_neds(venueName):
-    odds_arr = get_odds(venueName) # try getting the odds
-    if(len(odds_arr) == 0): # try a second time if result is empty
-        odds_arr = get_odds(venueName)
-    print(odds_arr)
-
-    ## Check if there 6 runners
-    if len(odds_arr) != 6:
-        print('Length of odds_arr = ' + str(len(odds_arr)))
-        # return -2
-
-    ## Sort out the odds_arr
-    sorted_odds = sorted(odds_arr,key=float)
-    print(sorted_odds)
-    pos1 = odds_arr.index(sorted_odds[0]) 
-    pos2 = odds_arr.index(sorted_odds[1]) 
-    pos3 = odds_arr.index(sorted_odds[2]) 
-    print("Forecast = " + str(pos1+1) + "-" + str(pos2+1))
-
-    ## TODO: check only if the 2 lowest odds are similar
-    if pos1 == pos2 or pos2 == pos3:
-        print('Two of the 3 smallest odds are the same : pos1 = ' + str(pos1) + ',pos2 = ' + str(pos2)+ ',pos3 = ' + str(pos3))
-        return -1
-
-    ## Choose the index of the lay selection out of 30 options or less
-    coordinates = []
-
-    for x in range(6):
-        for y in range(6):
-            if(x != y):
-                coordinates.append((x, y))
-
-    # res = [x+1 for x,y in enumerate(coordinates) if (y[0] ==pos1 and y[1] == pos2) ]
-    res = [x for x,y in enumerate(coordinates) if (y[0] ==pos1 and y[1] == pos2) ]
-    print(res[0])
-    return res[0]
-
-def get_next_market(market_catalogues):
-    timeNow = (datetime.datetime.now(pytz.timezone("Europe/London"))-datetime.timedelta(hours=1)) ## minus 1 hr due to daylight savings maybe?
-    print("Time Now: ")
-    print(timeNow.strftime("%Y-%m-%d %T"))
-    ## TODO Compare the list of markets - if timeNow is less than market start time (ie. market hasn't started yet), pick it. Otherwise, skip
-    time1 = timeNow.replace(tzinfo=pytz.UTC)
-
-    myRaceID = 0
-    myRaceVenue = ""
-    for marketObj in market_catalogues:
-        time2 = marketObj.market_start_time.replace(tzinfo=pytz.UTC)
-        if time1 < time2:
-            ## Found the race to bet
-            myRaceID = marketObj.market_id # store the market id
-            myRaceVenue = marketObj.event.venue.lower().replace(" ", "-") # store the venue
-            print("Found the market to lay: Name = " + myRaceVenue + " id = " + str(myRaceID))
-
-            print("market Name = ", marketObj.market_name)
-            break
-        
-    print("Market Start Time: " + str(time2))
-    
-    ##TODO Sleep until x seconds before the start time
-    return time2, time2-time1, myRaceID, myRaceVenue
-
-def get_runner1_name(market_catalogues):
-    return market_catalogues[0].runners[0].runner_name
-
-def get_new_price(prev_price):
-    prev_price = float(prev_price)
-    ## Valid increment options by group defined by Betfair 
-    increment_options = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10]
-
-    if prev_price <= 2:
-        increment = increment_options[0]
-    elif prev_price <= 3:
-        increment = increment_options[1]
-    elif prev_price <= 4:
-        increment = increment_options[2]
-    elif prev_price <= 6:
-        increment = increment_options[3]
-    elif prev_price <= 10:
-        increment = increment_options[4]
-    elif prev_price <= 20:
-        increment = increment_options[5]
-    elif prev_price <= 30:
-        increment = increment_options[6]
-    else:
-        print("Invalid price increment")
-        assert()
-    
-    return str(prev_price + increment)
 
 ## Write result to a csv file
 def write_to_file(fname, myRaceID, betOutcome, profit):
