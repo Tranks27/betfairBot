ace4661dd351fd89e4c38d6dcf9c3232487d6f67
Tanks_Dell_Latitude <tranksntb@gmail.com>
2022-07-16 08:31:54 +1000

    avoid more than 1 scratch

--- constants.py
+++ constants.py
@@ -6,6 +6,6 @@ API_KEY_DEMO = "X9EGwt5zbpg8o5xk"
 API_KEY_LIVE = "XzTt9hf8DAsmOuUa"
 
 GH_RACING_ID = 4339
-PREBET_DELAY = 20 # 20 seconds before bet is placed
+PREBET_DELAY = 15 # 20 seconds before bet is placed
 F_NAME = 'Profit_Loss.csv'
-NUM_GAMES = 50
+NUM_GAMES = 100
